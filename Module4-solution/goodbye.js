e9e2c1c0ad7a616a68672e2e139b8a7cf3de52a5
KUMARI03 <63048966+KUMARI03@users.noreply.github.com>
2020-07-07 16:56:16 +0530

    Update goodbye.js

--- Module4-solution/goodbye.js
+++ Module4-solution/goodbye.js
@@ -5,8 +5,8 @@ var speakword="Good Bye";
 var byespeaker={
 speak:function(name)
 {
-document.write(speakword+" "+name);
-document.write("<br>");
+console.log(speakword+" "+name);
+
 }
 };
 window.byespeaker=byespeaker;
